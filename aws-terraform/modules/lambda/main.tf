9ffb546915c88bec12bed5126a9997111bca3736
Kaylen Anderson <129983434+Xaidor@users.noreply.github.com>
2025-01-11 14:10:52 -0500

    python version needs to be updated

--- aws-terraform/modules/lambda/main.tf
+++ aws-terraform/modules/lambda/main.tf
@@ -1,5 +1,9 @@
 resource "aws_lambda_function" "gd_function" {
   function_name =  "gd-lambda-function"
-
+  runtime = python #grab python version 
+  handler = "gameday.lambda_handler"
+  filename = "${aws-terraform/modules/lambda/gameday.py}/function.zip"
+  source_code_hash = filebase64sha256("${aws-terraform/modules/lambda/gameday.py}/function.zip")
+  
   role = var.role
 }
