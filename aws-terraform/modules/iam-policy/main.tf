78f704525e7473b5523a8864e9ea8eed89c5b560
Kaylen Anderson <129983434+Xaidor@users.noreply.github.com>
2025-01-12 14:00:11 -0500

    policy attatchment

--- aws-terraform/modules/iam-policy/main.tf
+++ aws-terraform/modules/iam-policy/main.tf
@@ -1,17 +1,38 @@
 resource "aws_iam_policy" "sns_policy" {
   name        = "gd_sns_policy"
-  path        = "/TestGameDayPolicy"
+  path        = "/test-game-day-policy/" #A great way to keep things organized for multi-environment setups, Not really needed for smaller projects. 
   description = "Game Day SNS Policy for Lambda function"
 
-  
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Action = "sns:Publish",
-        Effect   = "Allow",
-        Resource = "${var.gd_arn}"
-      },
+        Action   = "sns:Publish"
+        Effect   = "Allow"
+        Resource = var.gd_arn 
+      }
     ]
   })
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name = "test_lambda_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com" 
+        }
+        Action = "sts:AssumeRole" 
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "sns_publish_attachment" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.sns_policy.arn
 }
