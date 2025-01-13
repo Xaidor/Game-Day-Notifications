3bd021cbd30d98b5ee8dfa90af03c9dc61061d04
Kaylen Anderson <129983434+Xaidor@users.noreply.github.com>
2025-01-12 20:34:05 -0500

    root main.tf configured

--- aws-terraform/main.tf
+++ aws-terraform/main.tf
@@ -1,10 +1,20 @@
-module "gd-mod-sns-" {
+module "gd-mod-sns" {
     source = "./modules/sns"
     sub_email = var.subscriber_email
 } 
 
 module "mod-iam-sns" {
     source = "./modules/iam-policy"
-
     gd_arn = module.gd-mod-sns.sns_arn
+}
+
+module "lambda-function-mod" {
+    source = "./modules/lambda"
+    role = module.mod-iam-sns.Lambda_role_arn
+}
+
+module "EventBridge" {
+    source = "./modules/eventbridge"
+    sns_arn = module.gd-mod-sns.sns_arn
+    role_arn = module.mod-iam-sns.Lambda_role_arn
 }
